8e4dadee56031b5d85ab12af37ed67b90e68ba6c
Bridgette-theWebMaster <69090403+Bridgette-theWebMaster@users.noreply.github.com>
2020-10-13 09:24:51 -0600

    Prepared for Heroku

--- server.js
+++ server.js
@@ -7,14 +7,22 @@ const MOVIEDEX= require('./moviedex.json')
 
 const app = express()
 
+<<<<<<< HEAD
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
+=======
 app.use(morgan('dev'))
+>>>>>>> b2e49765b7da5bd04963d1ca4edd6254ade98698
 app.use(helmet())
 app.use(cors())
 app.use(function validationBearerToken(req, res, next) {
     const authToken= req.get('Authorization')
     const apiToken= process.env.API_TOKEN
     
+<<<<<<< HEAD
+=======
     console.log('validate bearer token middleware')
+>>>>>>> b2e49765b7da5bd04963d1ca4edd6254ade98698
 
     if(!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error : 'Unauthorized request'})
@@ -57,10 +65,26 @@ app.get('/movie', function handleGetMovie(req, res) {
         )
     }
     res.json(response)
+<<<<<<< HEAD
+})
+
+app.use((error, req, res, next) => {
+    let response
+    if(process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error'}}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
+=======
 })
 
 
 const PORT = 8000
+>>>>>>> b2e49765b7da5bd04963d1ca4edd6254ade98698
 app.listen(PORT, () => {
     console.log(`Server listening at https://localhost: ${PORT}`)
 })
